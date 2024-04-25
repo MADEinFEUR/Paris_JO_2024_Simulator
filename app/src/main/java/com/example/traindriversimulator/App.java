264d7a7d9a0c19a45736803c63907cc3163f9f31
sapin malicieux <arthur.foure78@gmail.com>
2024-04-25 19:15:50 +0200

    arthur test1

--- app/src/main/java/com/example/traindriversimulator/App.java
+++ app/src/main/java/com/example/traindriversimulator/App.java
@@ -57,8 +57,11 @@ public class App extends AppCompatActivity {
     }
 
     public void intro(View v){
-        finishActivity(1);
-        launchGameWindow();
+        Intent gamewindow = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            gamewindow = new Intent(this, MenuActivity.class);
+        }
+        startActivities(new Intent[]{gamewindow});
 
     }
 
--- app/src/main/java/com/example/traindriversimulator/MenuActivity.java
+++ app/src/main/java/com/example/traindriversimulator/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.traindriversimulator;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,33 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
 
+
+    }
+    public void lancerJouer(View v){
+        Intent gamewindow = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+            gamewindow = new Intent(this, GamesActivity.class);
+        }
+        startActivities(new Intent[]{gamewindow});
+    }
+
+    public void lancerBoutique(View v){
+
+    }
+
+    public void lancerMDJ(View v){
+
+    }
+
+    public void lancerOption(View v){
+
+    }
+
+    public void lancerCredit(View v){
+
+    }
+
+    public void lancerQuitter(View v){
 
     }
 }
