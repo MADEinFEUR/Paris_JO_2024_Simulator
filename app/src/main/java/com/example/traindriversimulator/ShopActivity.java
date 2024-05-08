937a1344389962da3d3597c49065399e29eaf69a
sapin malicieux <arthur.foure78@gmail.com>
2024-05-08 16:48:01 +0200

    boutique menu test2

--- app/src/main/java/com/example/traindriversimulator/ShopActivity.java
+++ app/src/main/java/com/example/traindriversimulator/ShopActivity.java
@@ -16,9 +16,8 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.shop_activity);
-
-
     }
+}
     /* private Button payPalButton;
 
     @Override
