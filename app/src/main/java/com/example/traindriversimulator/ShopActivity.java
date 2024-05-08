f025f56886f252afce8f166a98cd07c84517e190
sapin malicieux <arthur.foure78@gmail.com>
2024-05-08 16:43:59 +0200

    boutique menu test2

--- app/src/main/java/com/example/traindriversimulator/ShopActivity.java
+++ app/src/main/java/com/example/traindriversimulator/ShopActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.VideoView;
 import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ShopActivity extends AppCompatActivity {
@@ -19,6 +18,22 @@ public class ShopActivity extends AppCompatActivity {
         setContentView(R.layout.shop_activity);
 
 
-
     }
-}
+    /* private Button payPalButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        payPalButton = findViewById(R.id.imageView5);
+
+        payPalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String payPalUrl = "https://www.paypal.com/checkoutnow"; // Replace this with the actual PayPal payment URL
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(payPalUrl));
+                startActivity(intent);
+} */
+
+
