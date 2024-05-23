9befe346abe4338a2d5ba5ab9c3a51969278aeec
sapin malicieux <arthur.foure78@gmail.com>
2024-05-23 19:15:53 +0200

    muzik1 et winnie qui danse

--- app/src/main/java/com/example/traindriversimulator/MenuActivity.java
+++ app/src/main/java/com/example/traindriversimulator/MenuActivity.java
@@ -18,22 +18,65 @@ public class MenuActivity extends AppCompatActivity {
 
     public static int mapChoisi;
 
+    private MediaPlayer musicPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_activity);
-        //music sncf coupe decale
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sncf_remix_coupe_decale);
-        mediaPlayer.start();
-        //video winnie qui danse
+
+        // Musique de guedin
+        musicPlayer = MediaPlayer.create(this, R.raw.sncf_remix_coupe_decale);
+        musicPlayer.setLooping(true);
+        musicPlayer.start();
+
+
+        // Video de malade
         VideoView videoView = findViewById(R.id.videoView);
-        videoView.setVideoPath("android.ressource://"+getPackageName()+"/"+R.raw.winnie_dance);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.winnie_dance);
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
+        videoView.requestFocus();
+
+        // looping OnPreparedListener pour winnie et donald qui dansent
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+                mp.start();
+            }
+        });
+
+        // Assurez-vous que la vidéo boucle sans arrêter la musique
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
+
+        // Lance la video
+        videoView.start();
     }
 
+    // Pour éviter les fuites de mémoire, n'oubliez pas d'arrêter et de libérer les MediaPlayer instances
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (musicPlayer != null) {
+            musicPlayer.stop();
+            musicPlayer.release();
+            musicPlayer = null;
+        }
+    }
+    private void stopMusic() {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sncf_remix_coupe_decale);
+        mediaPlayer.stop();
+        mediaPlayer.release();
+    }
     public void lancerJouer(View v){
+        stopMusic();
 
         Intent gamewindow = null;
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
@@ -65,12 +108,19 @@ public class MenuActivity extends AppCompatActivity {
     }
 
     public void lancerCredit(View v){
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sncf_remix_coupe_decale);
+        mediaPlayer.stop();
+        mediaPlayer.release();
 
     }
 
     public void lancerQuitter(View v){
         moveTaskToBack(true);
         finishAndRemoveTask();
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.sncf_remix_coupe_decale);
+        mediaPlayer.stop();
+        mediaPlayer.release();
     }
 
 }
